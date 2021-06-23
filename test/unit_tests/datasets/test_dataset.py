db311347d1ec93886b1179abfe5653bca063df1e
Maciej Sliwowski <macieksliwowski@gmail.com>
2021-06-23 21:18:28 +0200

    removed trial_start/stop offset from create_windows_from_targets, improved ecog bci 4 dataset

--- test/unit_tests/datasets/test_dataset.py
+++ test/unit_tests/datasets/test_dataset.py
@@ -86,8 +86,6 @@ def dataset_target_time_series():
     concat_ds = BaseConcatDataset([base_dataset])
     windows_dataset = create_windows_from_target_channels(
         concat_ds,
-        trial_start_offset_samples=10,
-        trial_stop_offset_samples=1000-20,
         window_size_samples=100,
     )
 
@@ -96,7 +94,7 @@ def dataset_target_time_series():
 
 def test_creating_windows_from_raw(dataset_target_time_series):
     _, target_positions, windows_dataset = dataset_target_time_series
-    len(windows_dataset)
+    assert len(windows_dataset) == 450
 
 
 def test_preprocessors(dataset_target_time_series):
