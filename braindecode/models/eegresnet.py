c964bd6a7daebba9d3cd6ed20587ee7aa5d9d2d2
Maciej Sliwowski <macieksliwowski@gmail.com>
2021-08-24 10:29:18 +0200

    Changed EEGResNet to send tensor to the correct device

--- braindecode/models/eegresnet.py
+++ braindecode/models/eegresnet.py
@@ -231,7 +231,7 @@ class _ResidualBlock(nn.Module):
             zeros_for_padding = torch.autograd.Variable(
                 torch.zeros(x.size()[0], self.n_pad_chans // 2, x.size()[2], x.size()[3]))
             if x.is_cuda:
-                zeros_for_padding = zeros_for_padding.cuda()
+                zeros_for_padding = zeros_for_padding.to(x.device)
             x = torch.cat((zeros_for_padding, x, zeros_for_padding), dim=1)
         out = self.nonlinearity(x + stack_2)
         return out
